f0e41cadeb71e135624a5f160c27b1be6c58ab6a
AgarOther <agarother@hotmail.com>
2025-06-29 17:20:37 +0200

    Database done

---
+++ rendu/backend/database/src/index.js
@@ -0,0 +1,48 @@
+import Fastify from 'fastify';
+import Bcrypt from 'bcrypt';
+import { initDatabase } from './db.js';
+import fs from 'fs';
+
+const fastify = Fastify({ logger: true });
+
+fastify.get('/api/database', async (request, reply) => { 
+  return { message: 'Hello from Database Service!' };
+});
+
+const start = async () => {
+	try {
+		const db = await initDatabase();
+
+		// Initialize db with sql file (if table is already created, does nothing)
+		const sql = fs.readFileSync('./init.sql').toString();
+		await db.exec(sql);
+
+		fastify.get('/api/database/users', async () => {
+			return await db.all('SELECT * FROM `users`');
+		});
+
+		fastify.post('/api/database/register', async (request, reply) => {
+			const { username, password } = request.body;
+			if (!username || !password)
+				return reply.status(400).send({error: 'Missing username and/or password.'});
+			try
+			{
+				const cryptedPass = await Bcrypt.hash(password, 10);
+				await db.run('INSERT INTO `users` (username, password) VALUES (?, ?)', [username, cryptedPass]);
+				reply.status(201).send({message: 'User registered.'});
+			}
+			catch (err)
+			{
+				reply.status(500).send({error: err.message});
+			}
+		});
+
+		await fastify.listen({ port: 3003, host: '0.0.0.0' });
+		console.log(`Database service listening on port 3003`);
+	} catch (err) {
+		fastify.log.error(err);
+		process.exit(1);
+	}
+};
+
+start();
