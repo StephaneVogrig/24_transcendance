e015a29f04105109f7dd8180d99d33a3a56c0e67
Gael Cannaud <gcannaud@3C3.42angouleme.fr>
2025-06-29 18:59:50 +0200

    mis a jour de l'arene

--- rendu/frontend/src/3d/meshes/arena.ts
+++ rendu/frontend/src/3d/meshes/arena.ts
@@ -1,5 +1,7 @@
 import { MeshBuilder, TransformNode, Scene, Color3, Vector3 } from "@babylonjs/core";
 import { createNeonMaterial } from "../materials/neon";
+import { createMetalMaterial } from "../materials/metal";
+import { createOrnement } from "./ornement";
 
 export function createArena(scene : Scene): TransformNode {
   const arena = new TransformNode("arena", scene);
@@ -37,10 +39,48 @@ export function createArena(scene : Scene): TransformNode {
     leftBottom.material = createNeonMaterial(scene, new Color3(0, 1, 1));
     leftBottom.position = new Vector3(-0.5, 0, 10);
 
+  const middleLine = MeshBuilder.CreateCylinder("arenaBase", {
+    diameter: 0.2,
+    height: 20,
+    tessellation: 64,
+  }, scene);
+    middleLine.material = createNeonMaterial(scene, new Color3(0, 1, 1));
+    middleLine.position = new Vector3(0, 0, 0);
+    middleLine.rotation.x = Math.PI / 2;
+
+  const goal1 = MeshBuilder.CreateCylinder("arenaBase", {
+    diameter: 0.2,
+    height: 20,
+    tessellation: 64,
+  }, scene);
+    goal1.material = createNeonMaterial(scene, new Color3(0, 1, 1));
+    goal1.position = new Vector3(0, 20, 0);
+    goal1.rotation.x = Math.PI / 2;
+
+  const goal2 = MeshBuilder.CreateCylinder("arenaBase", {
+    diameter: 0.2,
+    height: 20,
+    tessellation: 64,
+  }, scene);
+    goal2.material = createNeonMaterial(scene, new Color3(0, 1, 1));
+    goal2.position = new Vector3(0, -20, 0);
+    goal2.rotation.x = Math.PI / 2;
+    const ornementMiddle = createOrnement(scene);
+    const ornementtop = createOrnement(scene);
+    ornementtop.position = new Vector3(0, -20, 0);
+    const ornementbottom = createOrnement(scene);
+    ornementbottom.position = new Vector3(0, 20, 0);
+
   rightTop.parent = arena;
   leftTop.parent = arena;
   rightBottom.parent = arena;
   leftBottom.parent = arena;
+  middleLine.parent = arena;
+  goal1.parent = arena;
+  goal2.parent = arena;
+  ornementMiddle.parent = arena;
+  ornementtop.parent = arena;
+  ornementbottom.parent = arena;
   arena.rotation.z = Math.PI / 2;
   return arena;
 }
---
+++ rendu/frontend/src/3d/meshes/ornement.ts
@@ -0,0 +1,29 @@
+import { MeshBuilder, TransformNode, Scene, Color3, Vector3 } from "@babylonjs/core";
+import { createMetalMaterial } from "../materials/metal";
+
+export function createOrnement(scene : Scene): TransformNode {
+  const ornement = new TransformNode("ornement", scene);
+
+  const ornementRight = MeshBuilder.CreateCylinder("ornement", {
+    diameter: 2.0,
+    height: 0.5,
+    tessellation: 64,
+  }, scene);
+  ornementRight.material = createMetalMaterial(scene);
+  ornementRight.position = new Vector3(0, 0, -10);
+  ornementRight.rotation.x = Math.PI / 2;
+
+    const ornementLeft = MeshBuilder.CreateCylinder("ornement", {
+        diameter: 2.0,
+        height: 0.5,
+        tessellation: 64,
+    }, scene);
+    ornementLeft.material = createMetalMaterial(scene);
+    ornementLeft.position = new Vector3(0, 0, 10);
+    ornementLeft.rotation.x = Math.PI / 2;
+
+  ornementLeft.parent = ornement;
+  ornementRight.parent = ornement;
+  ornement.rotation.z = Math.PI / 2;
+  return ornement;
+}
