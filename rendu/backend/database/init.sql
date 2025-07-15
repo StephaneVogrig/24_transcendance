b569dc9e579911aca9c80791380d7d055174493f
Stephanie Mortemousque <smortemo@1E1.42angouleme.fr>
2025-07-15 10:50:29 +0200

    transfert users -> db

--- rendu/backend/database/init.sql
+++ rendu/backend/database/init.sql
@@ -1,8 +1,18 @@
+-- CREATE TABLE IF NOT EXISTS `users` (
+-- 	`id` INTEGER PRIMARY KEY AUTOINCREMENT,
+-- 	`username` TEXT UNIQUE NOT NULL,
+-- 	`password` TEXT NOT NULL
+-- );
 CREATE TABLE IF NOT EXISTS `users` (
 	`id` INTEGER PRIMARY KEY AUTOINCREMENT,
-	`username` TEXT UNIQUE NOT NULL,
-	`password` TEXT NOT NULL
+	`nickname` TEXT UNIQUE NOT NULL,
+	`password` TEXT, -- Nullable pour les utilisateurs OAuth
+	`email` TEXT NOT NULL UNIQUE,
+	`picture` TEXT, -- URL de l'avatar
+	`provider` TEXT DEFAULT 'local' NOT NULL, -- 'local', 'google', 'auth0', etc.
+	`provider_id` TEXT UNIQUE -- ID Auth0 unique
 );
+
 
 CREATE TABLE IF NOT EXISTS `tournaments` (
 	`id` INTEGER PRIMARY KEY,
