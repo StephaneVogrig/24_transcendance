feeae505e2e493af658edf840223e9e6f2457d1d
Gael Cannaud <gcannaud@1A2.42angouleme.fr>
2025-07-16 12:50:20 +0200

    fix warning

--- rendu/frontend/src/3d/materials/metal.ts
+++ rendu/frontend/src/3d/materials/metal.ts
@@ -3,11 +3,11 @@ import { Scene, CubeTexture, Color3, PBRMaterial } from '@babylonjs/core';
 export function createMetalMaterial(scene: Scene): PBRMaterial {
   const metalMat = new PBRMaterial("metalMat", scene);
 
-  if (!scene.environmentTexture) {
-    scene.environmentTexture = CubeTexture.CreateFromPrefilteredData(
-      "public/assets/sky.env", scene
-    );
-  }
+  // if (!scene.environmentTexture) {
+  //   scene.environmentTexture = CubeTexture.CreateFromPrefilteredData(
+  //     "public/assets/sky.env", scene
+  //   );
+  // }
   metalMat.albedoColor = new Color3(0.1, 0.1, 0.1);
   metalMat.metallic = 1.0;
   metalMat.roughness = 0.0;
