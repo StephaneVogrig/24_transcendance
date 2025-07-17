9694a97e8f97e25e0b25c283b28b70f76b580069
Gael Cannaud <gcannaud@1E2.42angouleme.fr>
2025-07-17 17:46:27 +0200

    add metal reflection

--- rendu/frontend/src/3d/materials/metal.ts
+++ rendu/frontend/src/3d/materials/metal.ts
@@ -3,14 +3,14 @@ import { Scene, CubeTexture, Color3, PBRMaterial } from '@babylonjs/core';
 export function createMetalMaterial(scene: Scene): PBRMaterial {
   const metalMat = new PBRMaterial("metalMat", scene);
 
-  // if (!scene.environmentTexture) {
-  //   scene.environmentTexture = CubeTexture.CreateFromPrefilteredData(
-  //     "public/assets/sky.env", scene
-  //   );
-  // }
-  metalMat.albedoColor = new Color3(0.1, 0.1, 0.1);
-  metalMat.metallic = 1.0;
-  metalMat.roughness = 0.0;
+  if (!scene.environmentTexture) {
+    scene.environmentTexture = CubeTexture.CreateFromPrefilteredData(
+      "public/assets/sky.env", scene
+    );
+  }
+  metalMat.albedoColor = new Color3(1, 1, 1);
+  metalMat.metallic = 0.95;
+  metalMat.roughness = 0.01;
 
   return metalMat;
 }
