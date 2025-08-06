d4c57d54cf9c686eb3ea3d6e226e903fd1b8cd8a
StephaneVogrig <stephane.vogrig@gmail.com>
2025-08-06 20:00:24 +0200

    optimisation et reduction du temps de stop et restart

--- rendu/backend/shared/server.js
+++ rendu/backend/shared/server.js
@@ -11,4 +11,31 @@ const start = async () => {
 	}
 };
 
+const gracefulShutdown = async (signal) => {
+	app.log.info(`Received ${signal}, shutting down gracefully`);
+
+	try {
+		await app.close();
+		app.log.info('Server closed successfully');
+		process.exit(0);
+	} catch (error) {
+		app.log.error('Error during shutdown:', error);
+		process.exit(1);
+	}
+};
+
+process.on('SIGTERM', () => gracefulShutdown('SIGTERM'));
+
+process.on('SIGINT', () => gracefulShutdown('SIGINT'));
+
+process.on('uncaughtException', (error) => {
+	app.log.error('Uncaught Exception:', error);
+	gracefulShutdown('uncaughtException');
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+	app.log.error('Unhandled Rejection at:', promise, 'reason:', reason);
+	gracefulShutdown('unhandledRejection');
+});
+
 start();
